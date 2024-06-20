464a0277d09d1797f1a13f2ba3efc998c186d2a5
TayYikoon <yikoon.23@ichat.sp.edu.sg>
2024-06-20 17:35:39 +0800

    Set up database

---
+++ backend/src/configs/initTables.js
@@ -0,0 +1,75 @@
+const pool = require("../services/db");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const callback = (error, results, fields) => {
+  if (error) {
+    console.error("Error creating tables:", error);
+  } else {
+    console.log("Tables created successfully");
+  }
+  process.exit();
+}
+
+bcrypt.hash('1234', saltRounds, (error, hash) => {
+  if (error) {
+    console.error("Error hashing password:", error);
+  } else {
+    console.log("Hashed password:", hash);
+
+    const SQLSTATEMENT = `
+    DROP TABLE IF EXISTS teachers;
+
+    DROP TABLE IF EXISTS students;
+    
+    DROP TABLE IF EXISTS materials;
+
+    DROP TABLE IF EXISTS worksheets;
+
+    CREATE TABLE Teachers (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password_hash TEXT NOT NULL,
+        subject TEXT,
+        class TEXT,
+        UNIQUE (email)
+    );
+
+    CREATE TABLE Students (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        class TEXT NOT NULL,
+        proficiency JSON NOT NULL,
+        UNIQUE (email)
+    );
+
+    CREATE TABLE Materials (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        teacher_id INT NOT NULL,
+        title TEXT NOT NULL,
+        content TEXT,
+        file_path TEXT,
+        tags JSON,
+        FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+    );
+
+    CREATE TABLE Worksheets (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      student_id INT NOT NULL,
+      teacher_id INT NOT NULL,
+      content TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (student_id) REFERENCES students(id),
+      FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+    );
+
+    INSERT INTO Teachers (name, email, password_hash, subject, class) VALUES
+        ('admin', 'a@a.com', '${hash}', NULL, NULL);
+      `;
+
+    pool.query(SQLSTATEMENT, callback);
+  }
+});
