d4c98f32c661dba4912c829b7c1c00175582a150
TayYikoon <yikoon.23@ichat.sp.edu.sg>
2024-06-20 21:30:53 +0800

    Completed login & register

--- backend/src/models/teachersModel.js
+++ backend/src/models/teachersModel.js
@@ -16,194 +16,29 @@ module.exports.selectAll = (callback) =>
 }
 
 //////////////////////////////////////////////////////
-// SELECT USER BY TOKEN
+// SELECT TEACHER BY TOKEN
 //////////////////////////////////////////////////////
-module.exports.selectById = (data, callback) =>
+module.exports.selectByEmail = (data, callback) =>
 {
     const SQLSTATEMENT = `
         SELECT * FROM Teachers
-        WHERE id = ?;
+        WHERE email = ?;
         `
-    const VALUES = [data.id];
+    const VALUES = [data.email];
 
     pool.query(SQLSTATEMENT, VALUES, callback);
 }
 
-// //////////////////////////////////////////////////////
-// // SELECT USER BY TOKEN
-// //////////////////////////////////////////////////////
-// module.exports.selectByTokenId = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         SELECT * FROM User
-//         WHERE id = ?;
-
-//         SELECT points FROM Points
-//         WHERE user_id = ?;
-//         `;
-//     const VALUES = [data.user_id, data.user_id];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // SELECT USER BY USERNAME
-// //////////////////////////////////////////////////////
-// module.exports.selectByUsername = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         SELECT * FROM User
-//         WHERE username = ?;
-
-//         UPDATE User
-//         SET last_logged_on = CURRENT_TIMESTAMP
-//         WHERE username = ?;
-//         `;
-//     const VALUES = [data.username, data.username];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // SELECT USER BY USERNAME OR EMAIL
-// //////////////////////////////////////////////////////
-// module.exports.selectByUsernameOrEmail = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         SELECT * FROM User
-//         WHERE username = ? OR email = ?;
-//         `;
-//     const VALUES = [data.username, data.email];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-// //////////////////////////////////////////////////////
-// // DEFINE SELECT OPERATION FOR SELECTING USERNAME AND EMAIL
-// //////////////////////////////////////////////////////
-// module.exports.selectUsernameAndEmail = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         SELECT * FROM User WHERE username = ?;
-//         SELECT * FROM User WHERE email = ?;
-//         `;
-//     const VALUES = [data.username, data.email];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // SELECT USER BY USER ID
-// //////////////////////////////////////////////////////
-// module.exports.selectById = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         SELECT * FROM User
-//         WHERE id = ?;
-//         `;
-//     const VALUES = [data.user_id];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // SELECT USER BY USER ROLE
-// //////////////////////////////////////////////////////
-// module.exports.selectByRole = (callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         SELECT * FROM User
-//         WHERE role = "user";
-//         `;
-
-//     pool.query(SQLSTATEMENT, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // INSERT USER AND PASSWORD INTO DATABASE
-// //////////////////////////////////////////////////////
-// module.exports.insertUser = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         INSERT INTO User (username, email, password, role)
-//         VALUES (?, ?, ?, "user");
-//         `;
-//     const VALUES = [data.username, data.email, data.password];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // DEFINE SELECT OPERATION FOR SELECTING TASK BY USER ID
-// //////////////////////////////////////////////////////
-// module.exports.selectTaskIds = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         SELECT task_id
-//         FROM TaskProgress
-//         WHERE user_id = ?;
-//         `;
-//     const VALUES = [data.user_id];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // DEFINE INSERT OPERATION FOR INSERTING INTO POINTS TABLE
-// //////////////////////////////////////////////////////
-// module.exports.insertSinglePoints = (data, callback) => {
-//     const SQLSTATEMENT = `
-//         INSERT INTO Points (user_id, points)
-//         VALUES (?, 0);
-//     `;
-//     const VALUES = [data.user_id];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // DEFINE UPDATE OPERATION FOR UPDATING USER INFORMATION
-// //////////////////////////////////////////////////////
-// module.exports.updateById = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         UPDATE User
-//         SET username = ?,
-//             email = ?,
-//             updated_on = CURRENT_TIMESTAMP
-//         WHERE id = ?;
-//         `;
-//     const VALUES = [data.username, data.email, data.user_id];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // DEFINE DELETE OPERATION FOR DELETING USER AND CORRESPONDING POINTS
-// //////////////////////////////////////////////////////
-// module.exports.deleteById = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         DELETE FROM User
-//         WHERE id = ?;
-
-//         DELETE FROM Points
-//         WHERE user_id = ?;
-//         `;
-//     const VALUES = [data.user_id, data.user_id];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
-
-// //////////////////////////////////////////////////////
-// // DEFINE DELETE OPERATION FOR DELETING TASK PROGRESS BY USER ID
-// //////////////////////////////////////////////////////
-// module.exports.deleteTaskProgressById = (data, callback) =>
-// {
-//     const SQLSTATEMENT = `
-//         DELETE FROM TaskProgress
-//         WHERE user_id = ?;
-//         `;
-//     const VALUES = [data.user_id];
-
-//     pool.query(SQLSTATEMENT, VALUES, callback);
-// }
+//////////////////////////////////////////////////////
+// INSERT EMAIL AND PASSWORD INTO DATABASE
+//////////////////////////////////////////////////////
+module.exports.insertTeacher = (data, callback) =>
+    {
+        const SQLSTATEMENT = `
+            INSERT INTO Teachers (name, email, password_hash, subject, class)
+            VALUES (?, ?, ?, ?, ?);
+            `;
+        const VALUES = [data.name, data.email, data.password_hash, data.subject, data.class];
+    
+        pool.query(SQLSTATEMENT, VALUES, callback);
+    }
--- backend/src/routes/mainRoutes.js
+++ backend/src/routes/mainRoutes.js
@@ -7,6 +7,9 @@ const teachersRoutes = require('./teachersRoutes');
 // const studentsRoutes = require('./studentsRoutes');
 // const materialsRoutes = require('./materialsRoutes');
 // const worksheetsRoutes = require('./worksheetsRoutes');
+const teachersController = require('../controllers/teachersController')
+const bcryptMiddleware = require('../middlewares/bcryptMiddleware');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
 
 //////////////////////////////////////////////////////
 // CREATE ROUTER
@@ -18,10 +21,10 @@ const router = express.Router();
 //////////////////////////////////////////////////////
 
 // Route for user login, including middleware for authentication and token generation
-// router.post("/login", userController.login, bcryptMiddleware.comparePassword, jwtMiddleware.generateToken, jwtMiddleware.sendToken);
+router.post("/login", teachersController.login, bcryptMiddleware.comparePassword, jwtMiddleware.generateToken, jwtMiddleware.sendToken);
 
-// // Route for user registration, including middleware for checking duplicate username/email, password hashing, user creation, points creation, and token generation
-// router.post("/register", userController.checkUsernameOrEmailExist, bcryptMiddleware.hashPassword, userController.register, userController.createPoints, jwtMiddleware.generateToken, jwtMiddleware.sendToken);
+// Route for user registration, including middleware for checking duplicate username/email, password hashing, user creation, points creation, and token generation
+router.post("/register", teachersController.checkEmailExist, bcryptMiddleware.hashPassword, teachersController.register, jwtMiddleware.generateToken, jwtMiddleware.sendToken);
 
 //////////////////////////////////////////////////////
 // DEFINE ROUTES
