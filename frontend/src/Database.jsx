f6f5d4f57ab34e2dfc32887c5d4a693d62d6ee61
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2024-06-21 11:31:30 +0800

    feat(search)

--- frontend/src/Database.jsx
+++ frontend/src/Database.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './style.css';
+import data from "./TemplateData.json";
 
 const Database = ({ setCurrentPage }) => {
   const [materials, setMaterials] = useState([]); // Initialize as an empty array
   const [error, setError] = useState(null); // State to handle errors
   const [file, setFile] = useState(null); // State to handle file uploads
-
+  const [searchTerm, setSearchTerm] = useState("");
+  
   useEffect(() => {
     const fetchMaterials = async () => {
       try {
@@ -92,6 +95,34 @@ const Database = ({ setCurrentPage }) => {
         )}
       </div>
       <button className="backButton" onClick={() => setCurrentPage('home')}>Back</button>
+      <div className="templateContainer">
+        <div className="searchInput_Container">
+          <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }} />
+        </div>
+        <div className="template_Container">
+          {
+            data 
+              .filter((val) => {
+                if(searchTerm == ""){
+                  return val;
+                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val;
+                }
+              })
+              .map((val) => {
+                return(
+                  <div className="template" key={val.id}>
+                      <img src={val.image} alt="" />
+                      <h3>{val.title}</h3>
+                      <p className="price">{val.who}</p>
+                  </div> 
+                )
+              })
+          }
+        </div>
+      </div>
     </div>
   );
 };
